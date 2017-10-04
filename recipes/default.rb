d7df93de0305290ad5fe9d94986eee18e1f0076e
Jane Smith <jsmith@example.com>
2017-10-04 03:07:34 +0000

    error in recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,7 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-package 'httpd'
-# Fising the error code ssdcd dv
+package 'httpd' do
+
+Fising the error code ssdcd dv
 # before approval ???? check if you see this line before approval.
