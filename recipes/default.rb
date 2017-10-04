82a9764d2259d02eb3fdebbb85c521d3c77a4a7c
Jane Smith <jsmith@example.com>
2017-10-04 02:52:24 +0000

    cookbooks

---
+++ recipes/default.rb
@@ -0,0 +1,9 @@
+#
+#
+# Cookbook:: scmtest
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+package 'httpd'
+# Fising the error code ssdcd dv
+# before approval ???? check if you see this line before approval.
